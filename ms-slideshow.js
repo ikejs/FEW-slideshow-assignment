c39bb03969d4523f736bf104fa1259d03f50c10d
Ike <ikescode@gmail.com>
2019-11-14 20:08:39 -0800

    tutorial 8

---
+++ ms-slideshow.js
@@ -0,0 +1,30 @@
+(function() {
+    function makeSlideshow(slides) {
+        // const slides = document.getElementById(slidesId)
+        const slidesInner = slides.querySelector('.slides-inner')
+        const images = slidesInner.querySelectorAll('img')
+
+        const delay = parseInt(slides.dataset.delay)
+        const transition = parseInt(slides.dataset.transition)
+        slidesInner.style.transition = `${transition}ms`
+
+
+        const slidesWidth = slides.clientWidth
+        let index = 0
+
+        setInterval(function() {
+            index += 1
+            if(index === images.length) {
+                index = 0
+            }
+
+            slidesInner.style.transform = `translate3d(${index * -slidesWidth}px, 0, 0)`
+
+        }, delay)
+    } //end makeSlideshow
+
+    const slideshows = document.querySelectorAll('.ms-slideshow')
+    for(let i=0; i < slideshows.length; i += 1) {
+        makeSlideshow(slideshows[i]);
+    }
+})()
